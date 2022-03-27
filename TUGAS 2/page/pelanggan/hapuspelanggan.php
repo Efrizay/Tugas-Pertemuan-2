677ed2605b4de4720f6c6090c449cd9ef8315423
Efrizay <101787530+Efrizay@users.noreply.github.com>
2022-03-27 20:31:17 +0700

    commit

--- TUGAS 2/page/pelanggan/hapuspelanggan.php
+++ TUGAS 2/page/pelanggan/hapuspelanggan.php
@@ -1,11 +1,10 @@
 <?php 
-  // Jika tidak ada id di url
+
   if (!isset($_GET['id'])) {
     header("Location: ?page=Pelanggan");
     exit;
   }
 
-  // Mengambil ID dari URL
   $id = $_GET['id'];
 
   if (deleteData('tbl_pelanggan', $id) > 0) {
