9ddb910d464f2aea3ccfb0dc87fb23b4451229bb
Efrizay <101787530+Efrizay@users.noreply.github.com>
2022-03-27 20:29:17 +0700

    commit

--- TUGAS 2/page/paket/paket.php
+++ TUGAS 2/page/paket/paket.php
@@ -1,4 +1,3 @@
-<!-- START: Content -->
 <div class="container">
 
   <div class="card mt-4 mb-4">
@@ -6,14 +5,14 @@
       <h5>Paket</h5>
     </div>
     <div class="card-body">
-      <!-- START: Button -->
+     
       <div class="d-flex justify-content-start mb-4">
         <a href="?page=TambahPaket" type="button" class="btn btn-sm btn-primary mr-3"><i
             class="fas fa-plus fa-sm text-white"></i> Tambah Data</a>
         <a href="page/paket/laporanpaket.php" target="_blank" type="button" class="btn btn-sm btn-info mr-3"><i
             class="fas fa-download fa-sm text-white"></i> Hasilkan PDF</a>
       </div>
-      <!-- END: Button -->
+
       <table id="dataTables" class="table table-hover">
         <thead>
           <tr>
@@ -61,4 +60,3 @@
   </div>
 
 </div>
-<!-- END: Content -->
